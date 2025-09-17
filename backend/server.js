cd4002db3f8cc0d15c679adbb7de40e480ecb470
NithinRegidi <regidinithinsainaidu@gmail.com>
2025-09-17 14:18:18 +0530

    Adding Controllers, Middlewares, Route for register, login, logout, get Current user details

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 connectDB();
@@ -21,6 +22,10 @@ app.use(cors({
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+
+app.use("/api/auth", authRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
